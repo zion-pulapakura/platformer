30efc39c02a0aae1122bea3f1ca33191a70ecdb8
Zion Pulapakura <pulapakurazion@gmail.com>
2024-12-17 21:11:04 +1100

    setup basic pygame window

---
+++ game.py
@@ -0,0 +1,17 @@
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode([1000, 500])
+
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((255, 255, 255))
+
+    pygame.display.flip()
+
+pygame.quit()
