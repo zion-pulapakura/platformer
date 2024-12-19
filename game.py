28a6126e2a5ce4344d2a503fb529404167bbdf9b
Zion Pulapakura <pulapakurazion@gmail.com>
2024-12-19 14:49:01 +1100

    converted the model from functions and while loop to a class

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import pygame
 import ctypes
 
+from hand_detection import HandDetector
+
 pygame.init()
 
 user32 = ctypes.windll.user32
@@ -8,6 +10,8 @@ width, height = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 
 screen = pygame.display.set_mode([0.5 * width, 0.6 * (height-60)])
 # minus 60 to account for the title bar
+
+model = HandDetector()
 
 running = True
 while running:
@@ -21,3 +25,4 @@ while running:
     pygame.display.flip()
 
 pygame.quit()
+model.stop()
--- hand_detection.py
+++ hand_detection.py
@@ -4,51 +4,50 @@ import mediapipe as mp
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 
-model = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.8)
-cap = cv2.VideoCapture(0)
+class HandDetector:
+    def __init__(self, detection_confidence=0.8, num_hands=1):
+        self.model = mp_hands.Hands(max_num_hands=num_hands, min_detection_confidence=detection_confidence)
 
-last_pos = None
+        self.cap = cv2.VideoCapture(0)
+        self.last_pos = None
 
-def hand_movement(hand_landmarks):
-    global last_pos
-    
-    landmarks = hand_landmarks.landmark
+    def stop(self):
+        self.cap.release()
+        cv2.destroyAllWindows()
 
-    # getting avg of all the landmarks
-    curr_pos = round(sum([landmark.x for landmark in landmarks]) / len(landmarks), 3)
+    def detect_hand_movement(self, hand_landmarks):    
+        landmarks = hand_landmarks.landmark
 
-    if last_pos is not None:
+        # getting avg of all the landmarks
+        curr_pos = round(sum([landmark.x for landmark in landmarks]) / len(landmarks), 3)
+
+        if self.last_pos is not None:
+
+            # moving left means the pos value decreases so the cur_pos would be smaller
+            if (self.last_pos - curr_pos) >= 0.01:
+                print("Moving left")
+
+            # moving right means the pos value increases so the cur_pos would be greater
+            elif (curr_pos - self.last_pos) >= 0.01:
+                print("Moving right")
+
+        self.last_pos = curr_pos
+
+    def run(self):
+        success, frame = self.cap.read()
+
+        if not success or cv2.waitKey(1) == ord('q'):
+            return None
+
+        # flipping the frame returns how the pic should look in real life, as the webcam will show a flipped video
+        reg_frame = cv2.flip(frame, 1)
+        frame_rgb = cv2.cvtColor(reg_frame, cv2.COLOR_BGR2RGB)
+        results = self.model.process(frame_rgb)
+
+        if results.multi_hand_landmarks:  
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(reg_frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                self.detect_hand_movement(hand_landmarks)
+
+        return frame
         
-        # moving left means the pos value decreases so the cur_pos would be smaller
-        if (last_pos - curr_pos) >= 0.01:
-            print("Moving left")
-
-        # moving right means the pos value increases so the cur_pos would be greater
-        elif (curr_pos - last_pos) >= 0.01:
-            print("Moving right")
-    
-    last_pos = curr_pos
-
-while cap.isOpened():
-    success, frame = cap.read()
-    if not success:
-        break
-
-    # flipping the frame returns how the pic should look in real life, as the webcam will show a flipped video
-    reg_frame = cv2.flip(frame, 1)
-    frame_rgb = cv2.cvtColor(reg_frame, cv2.COLOR_BGR2RGB)
-    
-    results = model.process(frame_rgb)
-
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(reg_frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-            hand_movement(hand_landmarks)
-
-    cv2.imshow('Platformer', reg_frame)
-
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
