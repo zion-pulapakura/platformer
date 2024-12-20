b573d3ebe0705f2af18f9d86c3727d3f5612fef5
Zion Pulapakura <pulapakurazion@gmail.com>
2024-12-20 16:34:17 +1100

    created function which generates random coordinates for the platforms

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 from hand_detection import HandDetectorWindow
 
@@ -6,13 +7,22 @@ pygame.init()
 
 class Game:
     def __init__(self, width, height):
-        self.WIDTH = width
-        self.HEIGHT = height
-        self.SCREEN = pygame.display.set_mode([width, height])
+        self.WIDTH = int(width)
+        self.HEIGHT = int(height)
+        self.SCREEN = pygame.display.set_mode([self.WIDTH, self.HEIGHT])
 
         self.CAMERA = HandDetectorWindow(width=0.4 * width, height=0.4 * height)
 
         self.is_running = True
+
+    def gen_platforms_coords(self, num):
+        coords = [
+            (random.randrange(self.CAMERA.width, self.WIDTH), 
+            random.randrange(self.CAMERA.height, self.HEIGHT)) 
+            for _ in range(num)
+        ]
+
+        return coords
 
     def run(self):
         while self.is_running:
@@ -20,12 +30,14 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.is_running = False
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q:
-                        self.is_running=False 
+                # if event.type == pygame.KEYDOWN:
+                #     if event.key == pygame.K_q:
+                #         self.is_running=False 
       
             self.SCREEN.fill((255, 255, 255))        
-            frame, movement = self.CAMERA.start()       
+            frame, movement = self.CAMERA.start()    
+            print(self.gen_platforms_coords(5))
+
 
             if frame is None:
                 self.is_running = False
