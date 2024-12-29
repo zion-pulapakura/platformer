57006777fa170815eda1a241f297d3fea8b0f5c1
Zion Pulapakura <pulapakurazion@gmail.com>
2024-12-29 11:49:37 +1100

    drawing platform using an image instead, and rounding the corners

--- assets/platform.py
+++ assets/platform.py
@@ -1,4 +1,8 @@
 import pygame
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Platform(pygame.sprite.Sprite):
     def __init__(self, screen, width, height, *groups):
@@ -10,6 +14,18 @@ class Platform(pygame.sprite.Sprite):
         self.HEIGHT = height
 
     def draw(self, x, y):
-        pygame.draw.rect(self.SCREEN, (173, 186, 57), (x, y, self.WIDTH, self.HEIGHT), border_top_left_radius=10, border_top_right_radius=10) #green
-        pygame.draw.rect(self.SCREEN, (89,94,41), (x, y + self.HEIGHT * 1/5, self.WIDTH, self.HEIGHT * 1/5))
-        pygame.draw.rect(self.SCREEN, (122, 87, 64), (x, y + self.HEIGHT * 2/5, self.WIDTH, self.HEIGHT * 3/5), border_bottom_left_radius=10, border_bottom_right_radius=10)
+        base = getenv('BASE')
+        platform = pygame.image.load(f"{base}\\platformer\\resources\\platform.png").convert_alpha()
+        scaled = pygame.transform.scale(platform, (self.WIDTH, self.HEIGHT))
+
+        # for rounded corners
+        rounded_surface = pygame.Surface((self.WIDTH, self.HEIGHT), pygame.SRCALPHA)
+        pygame.draw.rect(rounded_surface, (255, 255, 255, 255), (0, 0, self.WIDTH, self.HEIGHT), border_radius=5)
+
+        scaled.blit(rounded_surface, (0, 0), special_flags=pygame.BLEND_RGBA_MIN)
+
+        print(scaled.get_width())
+
+        self.SCREEN.blit(scaled, (x, y))
+
+        # for x in range(0, self.WIDTH, platform.get_width()):
