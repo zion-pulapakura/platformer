ad40ff3bc8ecb9c0e30c696bfdd06f01f7bb745b
Zion Pulapakura <pulapakurazion@gmail.com>
2024-12-29 12:35:14 +1100

    made so that the platform img will not be distorted no matter how wide the platform is

--- assets/platform.py
+++ assets/platform.py
@@ -1,6 +1,7 @@
 import pygame
 from os import getenv
 from dotenv import load_dotenv
+from math import modf
 
 load_dotenv()
 
@@ -15,17 +16,24 @@ class Platform(pygame.sprite.Sprite):
 
     def draw(self, x, y):
         base = getenv('BASE')
-        platform = pygame.image.load(f"{base}\\platformer\\resources\\platform.png").convert_alpha()
-        scaled = pygame.transform.scale(platform, (self.WIDTH, self.HEIGHT))
+        platform_img = pygame.image.load(f"{base}\\platformer\\resources\\platform.png").convert()
 
-        # for rounded corners
+        og_width = platform_img.get_width()
+        pygame.transform.scale(platform_img, (og_width, self.HEIGHT)) # only scaling height
+
+
+        # instead of stretching the image to fit the width, we just display multiple platforms right next to each other uptil the width
+        num_loop = round(self.WIDTH / og_width)
+
+        platform = pygame.Surface((self.WIDTH, self.HEIGHT)).convert_alpha()
+
+        for i in range(int(num_loop) + 1):
+            platform.blit(platform_img, (og_width * i, 0))
+
+        # rounding the corners
         rounded_surface = pygame.Surface((self.WIDTH, self.HEIGHT), pygame.SRCALPHA)
         pygame.draw.rect(rounded_surface, (255, 255, 255, 255), (0, 0, self.WIDTH, self.HEIGHT), border_radius=5)
 
-        scaled.blit(rounded_surface, (0, 0), special_flags=pygame.BLEND_RGBA_MIN)
+        platform.blit(rounded_surface, (0, 0), special_flags=pygame.BLEND_RGBA_MIN)
 
-        print(scaled.get_width())
-
-        self.SCREEN.blit(scaled, (x, y))
-
-        # for x in range(0, self.WIDTH, platform.get_width()):
+        self.SCREEN.blit(platform, (x, y))
